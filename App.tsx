640afa8eeb8e7d80b2a6b0f87822e932b03841f9
John Ferlito <johnf@inodes.org>
2024-08-19 15:11:14 +1000

    Reproducible example

--- App.tsx
+++ App.tsx
@@ -5,15 +5,98 @@
  * @format
  */
 
-import React from 'react';
-import {
-  Text,
-} from 'react-native';
+import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AvoidSoftInput } from 'react-native-avoid-softinput';
 
-function App(): React.JSX.Element {
+import React, { useCallback } from 'react';
+import { Button, Text, View } from 'react-native';
+
+export const useAvoidSoftInput = () => {
+  const onFocusEffect = useCallback(() => {
+    AvoidSoftInput.setShouldMimicIOSBehavior(true);
+    AvoidSoftInput.setEnabled(true);
+    return () => {
+      AvoidSoftInput.setEnabled(false);
+      AvoidSoftInput.setShouldMimicIOSBehavior(false);
+    };
+  }, []);
+
+  useFocusEffect(onFocusEffect);
+};
+
+
+const mainStyle = {
+  flex: 1,
+};
+const HomeScreen = ({ navigation }) => {
   return (
-  <Text style={{ fontSize: 24, color: 'black' }}>Test</Text>
-  );
-}
+  <View style={mainStyle}>
+    <Text style={{ fontSize: 24 }}>Home Screen</Text>
+    <Button title="Go to First" onPress={() => navigation.navigate('First')} />
+  </View>
+);
+};
+
+const FirstScreen = ({ navigation }) => {
+  useAvoidSoftInput();
+  return (
+  <View style={mainStyle}>
+    <Text style={{ fontSize: 24 }}>First Screen</Text>
+    <Button title="Go to Second" onPress={() => navigation.navigate('Second')} />
+  </View>
+);
+};
+
+const SecondScreen = ({ navigation }) => {
+  useAvoidSoftInput();
+  return (
+  <View style={mainStyle}>
+    <Text style={{ fontSize: 24 }}>Second Screen</Text>
+    <Button title="Go to Third" onPress={() => navigation.navigate('Third')} />
+  </View>
+);
+};
+
+const ThirdScreen = () => {
+  return (
+  <View style={mainStyle}>
+    <Text style={{ fontSize: 24 }}>Third Screen</Text>
+  </View>
+);
+};
+
+
+const AppStack = createStackNavigator();
+
+const AppNavigator = () => (
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Home" component={HomeScreen} />
+        <AppStack.Screen name="First" component={FirstScreen} />
+        <AppStack.Screen name="Second" component={SecondScreen} />
+        <AppStack.Screen name="Third" component={ThirdScreen} />
+      </AppStack.Navigator>
+);
+
+const RootStack = createStackNavigator();
+
+const RootNavigator = () => (
+  <View style={{ flex: 1 }}>
+    <NavigationContainer>
+      <RootStack.Navigator
+        // key={authUser ? 'authenticated' : 'unauthenticated'}
+        screenOptions={{ headerShown: false }}
+      >
+        {/* {authUser ? ( */}
+          <AppStack.Screen name="App" component={AppNavigator} />
+        {/* ) : ( */}
+          {/* <Stack.Screen name="Auth" component={AuthNavigator} /> */}
+        {/* )} */}
+      </RootStack.Navigator>
+    </NavigationContainer>
+  </View>
+);
+
+const App = () => <RootNavigator />;
 
 export default App;
